c6f8ac188819c8a8ad8ec5318fbb4ef6a4429b82
unknown <chase.parks94@gmail.com>
2018-02-26 15:10:06 -0700

    Operations class updated

--- Input_Output.cs
+++ Input_Output.cs
@@ -4,19 +4,22 @@ namespace UV_Simulator
 {
     public class Input_Output : Operations
     {
+        //CONSTRUCTOR
         public Input_Output()
         {
-
+            
         }
+
+        //MEMBER FUNCTIONS
         //Read a word from the keyboard into a specific location in memory.
         public bool Read()
         {
 
         }
         //Writes number stored in memory location
-        public bool Write()
+        public string Write()
         {
-
+            
         }
     }
 }
--- Operations.cs
+++ Operations.cs
@@ -2,11 +2,21 @@
 
 namespace UV_Simulator
 {
+    //Operations is the base class for all the Operations in our Basic Machine Language
     public class Operations
     {
+        //DATA MEMBERS
+        private string[] memory_locations = new string[MEMORY];
+        private int instruction_counter;
+        private int accumulator;
+        //CONSTANT VARIABLES
+        public const int MEMORY = 100;
 
-        public Class1()
+
+        public Operations()
         {
+            instruction_counter = 0;
+            accumulator = 0;
         }
 
 
