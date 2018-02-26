61f7e3540e78a31fdec5ccd02c0935c3095a492d
unknown <chase.parks94@gmail.com>
2018-02-26 16:04:51 -0700

    Pulled form files from GUI folder

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UV_Simulator
+{
+	public partial class Form1 : Form
+	{
+        Operations test = new Operations(); 
+		public Form1()
+		{
+			InitializeComponent();
+		}
+        public Submit_Button_Clicked()
+        {
+            test.insert();
+        }
+	}
+}
