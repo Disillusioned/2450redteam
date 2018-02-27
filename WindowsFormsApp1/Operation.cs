91d7a92a9cb24c6d3cf50941abe9633a9417719d
unknown <chase.parks94@gmail.com>
2018-02-26 17:50:24 -0700

    Changes made

--- WindowsFormsApp1/Operation.cs
+++ WindowsFormsApp1/Operation.cs
@@ -8,5 +8,23 @@ namespace WindowsFormsApp1
 {
     class Operation
     {
+        //DATA MEMBERS
+        private string[] memory_locations = new string[MEMORY];
+        private int instruction_counter;
+        private int accumulator;
+        //CONSTANT VARIABLES
+        public const int MEMORY = 100;
+
+        public Operation()
+        {
+            instruction_counter = 0;
+            accumulator = 0;
+        }
+
+        public bool Read(string _input, int _location)
+        {
+            memory_locations[_location] = _input;
+            return true;
+        }
     }
 }
