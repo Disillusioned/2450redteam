f5d0ff5d4279b616359c15e7b2a108b9c770629b
unknown <chase.parks94@gmail.com>
2018-02-26 17:35:19 -0700

    Updated class

--- WindowsFormsApp1/Operations.cs
+++ WindowsFormsApp1/Operations.cs
@@ -1,8 +1,33 @@
 ﻿using System;
 
-public class Operations
+namespace WindowsFormsApp1
 {
-	public Operations()
-	{
-	}
+    //Operations is the base class for all the Operations in our Basic Machine Language
+    public class Operations
+    {
+        //DATA MEMBERS
+        private string[] memory_locations = new string[MEMORY];
+        private int instruction_counter;
+        private int accumulator;
+        //CONSTANT VARIABLES
+        public const int MEMORY = 100;
+
+
+        public Operations()
+        {
+            instruction_counter = 0;
+            accumulator = 0;
+        }
+
+        public bool Read(string _input, int _location)
+        {
+            memory_locations[_location] = _input;
+            return true;
+        }
+
+
+
+    }
 }
+
+
