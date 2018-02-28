bc7bf151de7fd8a093b583423b9123159df1e199
unknown <chase.parks94@gmail.com>
2018-02-28 16:54:05 -0700

    Changed Operation class to Basic Machine Language class

---
+++ WindowsFormsApp1/BasicMachineLanguage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+	class BasicMachineLanguage
+	{
+		//DATA MEMBERS
+		private string[] memory_locations = new string[MEMORY];
+		private int instruction_counter;
+		private int accumulator;
+        
+		//CONSTANT VARIABLES
+		public const int MEMORY = 100;
+
+		//CONSTRUCTOR
+		public BasicMachineLanguage()
+		{
+			instruction_counter = 0;
+			accumulator = 0;
+		}
+
+		//GETTERS & SETTERS
+		public void SetNextInstruction(string _instruction)
+		{
+			memory_locations[instruction_counter] = _instruction;
+			instruction_counter++;
+		}
+
+		public string GetNextInstruction()
+		{
+			if (memory_locations[instruction_counter] != " ")
+			{
+				return memory_locations[instruction_counter];
+			}
+			else
+				return "END";
+		}
+
+        public int GetProgramCtr()
+        {
+            return instruction_counter;
+        }
+
+        public void SetProgramCtr(int position)
+		{
+			instruction_counter = position;
+		}
+
+		public int GetAccumulator()
+		{
+			return accumulator;
+		}
+
+        public void SetAccumulator(int _value)
+        {
+            accumulator = _value;
+        }
+
+		public string GetInstructionAt(int _location)
+		{
+			return memory_locations[_location];
+		}
+
+        public void SetInstructionAt(int _location, string _instruction)
+        {
+            memory_locations[_location] = _instruction;
+        }
+
+		//ITERATOR
+		public void IncrementInstructionCtr()
+		{
+			++instruction_counter;
+		}
+        
+		
+	}
+}
