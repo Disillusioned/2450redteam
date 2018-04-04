d7c136413fe26295e50755bb3117d4c69f539204
Benny Yamagata Jr <benny.yamagata@gmail.com>
2018-04-03 22:10:55 -0600

    Finished divide, debug multiply
    Added divison helper recursive bitwise function.  Also added negative multiplication functionality.

--- WindowsFormsApp1/ALU.cs
+++ WindowsFormsApp1/ALU.cs
@@ -60,32 +60,102 @@ namespace WindowsFormsApp1
             bml.SetAccumulator(accumulator);
         }
 
-        public void MULTIPLY(int _location)
+        public void MULTIPLY(int _location) //handles negative and positive
         {
             int multiply = int.Parse(bml.GetInstructionAt(_location));
             int accumulator = bml.GetAccumulator();
+            if(multiply == 0 || accumulator == 0)
+            {
+                bml.SetAccumulator(0);
+            }
             int count = multiply - 1;
             int carry;
-            while(count != 0)
+
+            if (multiply < 0 || accumulator < 0)
             {
-                multiply = int.Parse(bml.GetInstructionAt(_location));
-                while(multiply != 0)
+                multiply = Math.Abs(multiply);
+                accumulator = Math.Abs(accumulator);
+
+                while (count != 0)
                 {
-                    carry = accumulator & multiply;
-                    accumulator = accumulator ^ multiply;
-                    multiply = carry << 1;
+                    multiply = int.Parse(bml.GetInstructionAt(_location));
+                    while (multiply != 0)
+                    {
+                        carry = accumulator & multiply;
+                        accumulator = accumulator ^ multiply;
+                        multiply = carry << 1;
+                    }
+                    --count;
                 }
-                --count;
+                bml.SetAccumulator(-accumulator);
             }
-            bml.SetAccumulator(accumulator);
+            else
+            {
+                while (count != 0)
+                {
+                    multiply = int.Parse(bml.GetInstructionAt(_location));
+                    while (multiply != 0)
+                    {
+                        carry = accumulator & multiply;
+                        accumulator = accumulator ^ multiply;
+                        multiply = carry << 1;
+                    }
+                    --count;
+                }
+                bml.SetAccumulator(accumulator);
+            }
         }
 
-        public void DIVIDE(int _location)
+        public void DIVIDE(int _location) //handles negative and postiive division
         {
-            int dividend = int.Parse(bml.GetInstructionAt(_location));
-            int accumulator = bml.GetAccumulator();
-            accumulator /= dividend;
-            bml.SetAccumulator(accumulator);
+            int remain = 0;
+            int dividend = bml.GetAccumulator();
+            int divisor = int.Parse(bml.GetInstructionAt(_location));
+            int quotient;
+            if(dividend < 0 || divisor < 0)
+            {
+                dividend = Math.Abs(dividend);
+                divisor = Math.Abs(divisor);
+                quotient = DivisonHelp(dividend, divisor, divisor, ref remain);
+                bml.SetAccumulator(-quotient);
+            }
+            else
+            {
+                quotient = DivisonHelp(dividend, divisor, divisor, ref remain);
+                bml.SetAccumulator(quotient);
+            }
+        }
+
+        public int DivisonHelp(int dividend, int divisor, int origdiv, ref int remain)
+        {
+            int quotient = 1;
+            if(dividend == divisor)
+            {
+                remain = 0;
+                return 1;
+            }
+            else if( dividend < divisor)
+            {
+                remain = dividend;
+                return 0;
+            }
+
+
+            while(divisor <= dividend)
+            {
+                divisor <<= 1;
+                quotient <<= 1;
+            }
+
+            if(dividend < divisor)
+            {
+                divisor >>= 1;
+                quotient >>= 1;
+            }
+
+            quotient = quotient + DivisonHelp(dividend - divisor, origdiv, origdiv, ref remain);
+
+            return quotient;
         }
     }
 }
